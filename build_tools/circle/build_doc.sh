2946d9ce028b0f67bed5f586ae50f4e9be73ccde
Chiara Marmo <chiara.marmo@inria.fr>
2019-10-24 13:19:38 +0200

    Fix bash syntax error.

--- build_tools/circle/build_doc.sh
+++ build_tools/circle/build_doc.sh
@@ -204,7 +204,7 @@ then
     echo '</ul></body></html>'
     ) > 'doc/_build/html/stable/_changed.html'
 
-    check = `echo $warnings | grep "no warnings"`
+    check=`echo $warnings | grep "no warnings"`
     if [ -z "$check" ]
     then
         echo "There are Sphinx Warnings in the documentation!"
